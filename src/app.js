c434270c3fa359e3f88e36632d106ee006ca0684
fgolemo <fgolemo@gmail.com>
2016-03-28 02:19:12 +0200

    added moving with WASD and adjusting orientation

--- src/app.js
+++ src/app.js
@@ -1,7 +1,55 @@
+var Movments = {
+    up: new cc.p(0, 1),
+    down: new cc.p(0, -1),
+    right: new cc.p(1, 0),
+    left: new cc.p(-1, 0)
+};
 
 var HelloWorldLayer = cc.Layer.extend({
-    sprite:null,
-    ctor:function () {
+    sprite: null,
+    moving: {
+        up: 0.0,
+        down: 0.0,
+        left: 0.0,
+        right: 0.0
+    },
+    player: null,
+    velocity: 500.0,
+    aim: null,
+    updatePosition: function (dt) {
+        var trajectory = cc.p(0.0, 0.0);
+        trajectory = cc.pAdd(trajectory, cc.pMult(Movments.up, this.moving.up));
+        trajectory = cc.pAdd(trajectory, cc.pMult(Movments.down, this.moving.down));
+        trajectory = cc.pAdd(trajectory, cc.pMult(Movments.right, this.moving.right));
+        trajectory = cc.pAdd(trajectory, cc.pMult(Movments.left, this.moving.left));
+        trajectory = cc.pMult(trajectory, dt * this.velocity);
+
+        var newPos = cc.pAdd(this.player.getPosition(), trajectory);
+
+        this.player.setPosition(newPos);
+    },
+    updateOrientation: function (dt) {
+        xDiff = this.aim._x - this.player.x;
+        yDiff = this.aim._y - this.player.y;
+        angleRad = Math.atan(yDiff / xDiff);
+        angleDeg = angleRad * 180 / Math.PI;
+        if (xDiff > 0 && yDiff < 0) {
+            angleDeg = -1 * angleDeg;
+        } else if (xDiff < 0 && yDiff <= 0) {
+            angleDeg = 180 - angleDeg;
+        } else if (xDiff < 0 && yDiff > 0) {
+            angleDeg = 180 - 1 * angleDeg;
+        } else if (xDiff >= 0 && yDiff > 0) {
+            angleDeg = 360 - angleDeg
+        }
+
+        this.player.setRotation(angleDeg);
+    },
+    update: function (dt) {
+        this.updatePosition(dt);
+        this.updateOrientation(dt);
+    },
+    ctor: function () {
         //////////////////////////////
         // 1. super init first
         this._super();
@@ -39,37 +87,52 @@ var HelloWorldLayer = cc.Layer.extend({
         this.player.setAnchorPoint(cc.p(0.25, 0.5));
         this.addChild(this.player, 10);
 
-
         var scope = this;
 
         cc.eventManager.addListener({
             event: cc.EventListener.MOUSE,
             onMouseMove: function (event) {
                 // console.log(event._x, event._y);
-                xDiff = event._x - scope.player.x;
-                yDiff = event._y - scope.player.y;
-                angleRad = Math.atan(yDiff/xDiff);
-                angleDeg = angleRad * 180 / Math.PI;
-                if (xDiff > 0 && yDiff < 0){
-                    angleDeg = -1 * angleDeg;
-                } else if (xDiff < 0 && yDiff <= 0) {
-                    angleDeg = 180 - angleDeg;
-                } else if (xDiff < 0 && yDiff > 0) {
-                    angleDeg = 180 - 1*angleDeg;
-                } else if (xDiff >= 0&& yDiff >0) {
-                    angleDeg = 360 - angleDeg
-                }
-                // console.log(xDiff, yDiff, angleDeg);
-                scope.player.setRotation(angleDeg);
+                scope.aim = event;
             }
         }, this);
+
+        var setMovement = function (keycode, up) {
+            switch (keycode) {
+                case 87:
+                    scope.moving.up = up;
+                    break;
+                case 83:
+                    scope.moving.down = up;
+                    break;
+                case 65:
+                    scope.moving.left = up;
+                    break;
+                case 68:
+                    scope.moving.right = up;
+                    break;
+            }
+        };
+
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function (keycode, event) {
+                setMovement(keycode, 1);
+            },
+            onKeyReleased: function (keycode, event) {
+                setMovement(keycode, 0);
+            }
+
+        }, this);
+
+        this.scheduleUpdate();
 
         return true;
     }
 });
 
 var HelloWorldScene = cc.Scene.extend({
-    onEnter:function () {
+    onEnter: function () {
         this._super();
         var layer = new HelloWorldLayer();
         this.addChild(layer);
